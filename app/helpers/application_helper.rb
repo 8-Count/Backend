a9da1af1c0c4670f39c46eb8f2e6581760a3aac5
schaudhry123 <schaudhry123@gmail.com>
2014-11-11 15:19:01 -0600

    Add tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis
   def full_title(page_title = '')                     # Method definition, parameter
-    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    base_title = "Ruby on Rails Sample App"  # Variable assignment
     if page_title.empty?                              # Boolean test
       base_title                                      # Return string
     else
