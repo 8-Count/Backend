74340b96662984c0726f4c5bf390663881f6d2fc
schaudhry123 <schaudhry123@gmail.com>
2014-11-28 10:33:21 -0800

    Finish secure remember sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124212143) do
+ActiveRecord::Schema.define(version: 20141128181439) do
 
   create_table "athletes", force: true do |t|
     t.integer  "user_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20141124212143) do
     t.integer  "userType"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
     t.string   "password_digest"
   end
 
