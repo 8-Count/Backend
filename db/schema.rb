58a06f785ccbcf5ac322db2904feecbd14a6bd56
schaudhry123 <schaudhry123@gmail.com>
2015-01-16 19:02:14 -0800

    Password reset w/o tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117020217) do
+ActiveRecord::Schema.define(version: 20150117023755) do
 
   create_table "athletes", force: true do |t|
     t.integer  "user_id"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 20150117020217) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
